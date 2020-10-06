026f7bc4b1e3d4d46d240aafb4c54474dd2ecc2b
Maharshi Rawal <rawalmaharshi@gmail.com>
2020-10-06 08:08:13 -0400

    multi-hashing: take input

--- HashingTables/src/MultiHashing.java
+++ HashingTables/src/MultiHashing.java
@@ -1,2 +1,20 @@
 public class MultiHashing {
+
+    public static void main(String[] args) {
+        int noOfEntries, noOfFlows, noOfHashes;
+        if (args.length == 3) {
+            try {
+                noOfEntries = Integer.parseInt(args[0]);
+                noOfFlows = Integer.parseInt(args[1]);
+                noOfHashes = Integer.parseInt(args[2]);
+                System.out.println("Table Size: " + noOfEntries + ", No. of Flows: " + noOfFlows + ", No.of Hash Functions: " + noOfHashes);
+            } catch (Exception e) {
+                System.err.println("Error parsing arguments. Expected an integer.");
+                System.exit(1);
+            }
+        } else {
+            System.err.println("Arguments mismatch. Expected 3 arguments.");
+            System.exit(1);
+        }
+    }
 }
