9ab25680fedfda594c6f05101f598e191d667159
Maharshi Rawal <rawalmaharshi@gmail.com>
2020-10-10 10:28:30 -0400

    minor change in MainFunction; add make file.

--- HashingTables/src/MainFunction.java
+++ HashingTables/src/MainFunction.java
@@ -47,12 +47,31 @@ public class MainFunction {
                 CuckooTable tab = new CuckooTable(noOfEntries, noOfHashes, noOfCuckooSteps);
                 tab.insert(flowArray, noOfCuckooSteps);
                 tab.print();
-            } else if (hashType.equalsIgnoreCase("all")) {  //implement all hashing algorithms
+            } else {
+                System.err.println("Error parsing arguments. Expected 'cuckoo' as 1st argument.");
+                System.exit(1);
+            }
+        } else if (args.length == 6) {
+            noOfEntries = Integer.parseInt(args[1]);
+            noOfFlows = Integer.parseInt(args[2]);
+            noOfHashes = Integer.parseInt(args[3]);
+            noOfCuckooSteps = Integer.parseInt(args[4]);
+            int noOfSegments = Integer.parseInt(args[5]);
+
+            //This array would be used for all hashing algorithms
+            int [] flowArray = new int[noOfFlows];
+            int min = 1, max = Integer.MAX_VALUE;
+            for (int i = 0; i < noOfFlows; i++) {
+                int flowID = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                flowArray[i] = flowID;
+            }
+
+            if (hashType.equalsIgnoreCase("all")) {  //implement all hashing algorithms
                 MultiTable mlt = new MultiTable(noOfEntries, noOfHashes);
                 mlt.insert(flowArray);
                 mlt.print();
 
-                DLeftTable dlft = new DLeftTable(noOfEntries, noOfHashes);
+                DLeftTable dlft = new DLeftTable(noOfEntries, noOfSegments);
                 dlft.insert(flowArray);
                 dlft.print();
 
@@ -60,7 +79,7 @@ public class MainFunction {
                 cko.insert(flowArray, noOfCuckooSteps);
                 cko.print();
             } else {
-                System.err.println("Error parsing arguments. Expected either of 'cuckoo' or 'all' as 1st argument.");
+                System.err.println("Error parsing arguments. Expected 'all' as 1st argument.");
                 System.exit(1);
             }
         } else {
