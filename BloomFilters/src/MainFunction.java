e58e108cabbb9d8515ae39a560f5f8908da04dd4
Maharshi Rawal <rawalmaharshi@gmail.com>
2020-10-19 01:52:20 -0400

    add mainfunction and makefile - complete

--- BloomFilters/src/MainFunction.java
+++ BloomFilters/src/MainFunction.java
@@ -1,90 +1,134 @@
+import java.io.FileWriter;
+
 public class MainFunction {
     public static void main(String[] args) {
         String filterType = args[0];
-        int noOfElements, noOfBits, noOfHashes;
-
+        int noOfElements, noOfBits, noOfHashes, noOfRemoveElements, noOfAddElements, noOfSets, noOfFilters;
         if (args.length == 4) { //bloom filter
-            noOfElements = Integer.parseInt(args[1]);
-            noOfBits = Integer.parseInt(args[2]);
-            noOfHashes = Integer.parseInt(args[3]);
+            try {
+                noOfElements = Integer.parseInt(args[1]);
+                noOfBits = Integer.parseInt(args[2]);
+                noOfHashes = Integer.parseInt(args[3]);
 
-            int [] flowArray = new int[noOfBits];
-            int min = 1, max = Integer.MAX_VALUE;
-            for (int i = 0; i < noOfBits; i++) {
-                int flowID = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
-                flowArray[i] = flowID;
+                int [] setA = new int[noOfElements], setB = new int [noOfElements];
+                int min = 1, max = Integer.MAX_VALUE;
+                for (int i = 0; i < noOfElements; i++) {
+                    int element1 = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                    int element2 = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                    setA[i] = element1;
+                    setB[i] = element2;
+                }
+
+                BloomFilterImpl filt = new BloomFilterImpl(noOfBits, noOfHashes);
+                filt.encode(setA);
+
+                try {
+                    FileWriter fw = null;
+                    String opFile = "bloom_filter_output.txt";
+                    fw = new FileWriter(opFile);
+                    fw.write("After lookup of elements in A, No. of elements in the filter: " + filt.lookup(setA) + "\n");
+                    fw.write("After lookup of elements in B, No. of elements in the filter: " + filt.lookup(setB));
+                    fw.close();
+                    System.out.println("Output in file: " + opFile);
+                } catch (Exception e) {
+                    System.err.println("Error printing to file. " + e);
+                }
+
+            } catch (Exception e) {
+                System.err.println("Error" + e);
+                System.exit(1);
             }
+        } else if (args.length == 6) {
+            if (filterType.equalsIgnoreCase("counting")) {
+                try {
+                    noOfElements = Integer.parseInt(args[1]);
+                    noOfRemoveElements = Integer.parseInt(args[2]);
+                    noOfAddElements = Integer.parseInt(args[3]);
+                    noOfBits = Integer.parseInt(args[4]);
+                    noOfHashes = Integer.parseInt(args[5]);
 
-//            if (filterType.equalsIgnoreCase("multi")) {
-//                MultiTable tab = new MultiTable(noOfElements, noOfHashes);
-//                tab.insert(flowArray);
-//                tab.print();
-//            }
-//
-//            else if (filterType.equalsIgnoreCase("dleft")) {
-//                DLeftTable tab = new DLeftTable(noOfElements, noOfHashes);
-//                tab.insert(flowArray);
-//                tab.print();
-//            } else {
-//                System.err.println("Error parsing arguments. Expected either of 'multi' or 'dleft' as 1st argument.");
-//                System.exit(1);
-//            }
-//        } else if (args.length == 5) {
-//            noOfElements = Integer.parseInt(args[1]);
-//            noOfBits = Integer.parseInt(args[2]);
-//            noOfHashes = Integer.parseInt(args[3]);
-//            noOfCuckooSteps = Integer.parseInt(args[4]);
-//
-//            //This array would be used for all hashing algorithms
-//            int [] flowArray = new int[noOfBits];
-//            int min = 1, max = Integer.MAX_VALUE;
-//            for (int i = 0; i < noOfBits; i++) {
-//                int flowID = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
-//                flowArray[i] = flowID;
-//            }
-//
-//            if (filterType.equalsIgnoreCase("cuckoo")) {
-//                CuckooTable tab = new CuckooTable(noOfElements, noOfHashes, noOfCuckooSteps);
-//                tab.insert(flowArray, noOfCuckooSteps);
-//                tab.print();
-//            } else {
-//                System.err.println("Error parsing arguments. Expected 'cuckoo' as 1st argument.");
-//                System.exit(1);
-//            }
-//        } else if (args.length == 6) {
-//            noOfElements = Integer.parseInt(args[1]);
-//            noOfBits = Integer.parseInt(args[2]);
-//            noOfHashes = Integer.parseInt(args[3]);
-//            noOfCuckooSteps = Integer.parseInt(args[4]);
-//            int noOfSegments = Integer.parseInt(args[5]);
-//
-//            //This array would be used for all hashing algorithms
-//            int [] flowArray = new int[noOfBits];
-//            int min = 1, max = Integer.MAX_VALUE;
-//            for (int i = 0; i < noOfBits; i++) {
-//                int flowID = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
-//                flowArray[i] = flowID;
-//            }
-//
-//            if (filterType.equalsIgnoreCase("all")) {  //implement all hashing algorithms
-//                MultiTable mlt = new MultiTable(noOfElements, noOfHashes);
-//                mlt.insert(flowArray);
-//                mlt.print();
-//
-//                DLeftTable dlft = new DLeftTable(noOfElements, noOfSegments);
-//                dlft.insert(flowArray);
-//                dlft.print();
-//
-//                CuckooTable cko = new CuckooTable(noOfElements, noOfHashes, noOfCuckooSteps);
-//                cko.insert(flowArray, noOfCuckooSteps);
-//                cko.print();
-//            } else {
-//                System.err.println("Error parsing arguments. Expected 'all' as 1st argument.");
-//                System.exit(1);
-//            }
-//        } else {
-//            System.err.println("Arguments mismatch. Please try again.");
-//            System.exit(1);
+                    CountingBFilterImpl cbf = new CountingBFilterImpl(noOfBits, noOfHashes);
+
+                    int [] setA = new int[noOfElements], setACopy = new int[noOfRemoveElements], setB = new int[noOfAddElements];
+                    int min = 1, max = Integer.MAX_VALUE;
+                    for (int i = 0; i < noOfElements; i++) {
+                        int element = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                        setA[i] = element;
+                    }
+                    //Encodes elements in the bloom filter
+                    cbf.encode(setA);
+
+                    //Remove elements
+                    for (int i = 0; i < noOfRemoveElements; i++) {
+                        setACopy[i] = setA[i]; //copies the elements from a
+                    }
+                    //remove call
+                    cbf.removeElem(setACopy);   //Removes the first 500(no of elements to remove) from set A
+
+                    //Add elements
+                    for (int i = 0; i < noOfAddElements; i++) {
+                        int element = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                        setB[i] = element;
+                    }
+                    //add call
+                    cbf.encode(setB);
+
+                    try {
+                        FileWriter fw = null;
+                        String opFile = "counting_bloom_filter_output.txt";
+                        fw = new FileWriter(opFile);
+                        fw.write("After lookup of elements in A, No. of elements in the filter: " + cbf.lookup(setA));
+                        fw.close();
+                        System.out.println("Output in file: " + opFile);
+                    } catch (Exception e) {
+                        System.err.println("Error printing to file. " + e);
+                    }
+
+                } catch (Exception e) {
+                    System.err.println("Error: " + e);
+                    System.exit(1);
+                }
+            } else if (filterType.equalsIgnoreCase("coded")) {
+                try {
+                    noOfSets = Integer.parseInt(args[1]);
+                    noOfElements = Integer.parseInt(args[2]);
+                    noOfFilters = Integer.parseInt(args[3]);
+                    noOfBits = Integer.parseInt(args[4]);
+                    noOfHashes = Integer.parseInt(args[5]);
+
+                    CodedBFilterImpl cbf = new CodedBFilterImpl(noOfSets, noOfFilters, noOfBits, noOfHashes);
+
+                    int [][] set = new int[noOfSets][noOfElements];
+                    int min = 1, max = Integer.MAX_VALUE;
+                    for (int i = 0; i < noOfSets; i++) {
+                        for (int j = 0; j < noOfElements; j++) {
+                            int element = (int) (Math.random() * (max - min)); //Returns value in the range 1 - Integer.MAX_VALUE
+                            set[i][j] = element;
+                        }
+                    }
+                    cbf.encode(set);
+
+                    try {
+                        FileWriter fw = null;
+                        String opFile = "coded_bloom_filter_output.txt";
+                        fw = new FileWriter(opFile);
+                        fw.write("Number of elements whose lookup results are correct: " + cbf.lookup(set));
+                        fw.close();
+                        System.out.println("Output in file: " + opFile);
+                    } catch (Exception e) {
+                        System.err.println("Error printing to file. " + e);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error: " + e);
+                    System.exit(1);
+                }
+            } else {
+                System.err.println("Error parsing arguments. Expected either of 'counting' or 'coded' as 1st argument.");
+                System.exit(1);
+            }
+        } else {
+            System.err.println("Arguments mismatch. Please try again.");
+            System.exit(1);
         }
     }
 }
